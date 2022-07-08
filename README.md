a57afeb94f722d5342958dc8122d30b59657d044
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2022-07-08 20:29:48 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prestashop module
+
+My first Prestashop module displaying a visit counter on home page.
---
+++ myfirstmodule.php
@@ -0,0 +1,50 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class MyFirstModule extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'myfirstmodule';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Jakub Pacanowski';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('My First module');
+        $this->description = $this->l('My first example module.');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        if (!Configuration::get('MYFIRSTMODULE_NAME'))
+            $this->warning = $this->l('No name provided');
+    }
+
+    public function install()
+    {
+        return parent::install()
+            && $this->registerHook('displayHome')
+            && Configuration::updateValue('licznik', 0);
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('licznik');
+        return parent::uninstall();
+    }
+
+    public function hookDisplayHome(array $params)
+    {
+        if(Tools::getValue('controller') == 'index') {
+            Configuration::updateValue('licznik', Configuration::get('licznik') + 1);
+            return 'Wejścia: ' . Configuration::get('licznik');
+        }
+    }
+}
